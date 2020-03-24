fef7fac81c6ff95a1c07715d31eeeed8d2105bb7
joo168 <qlsn96@gmail.com>
2020-03-25 00:23:31 +0900

    Updates

---
+++ precache-manifest.6ecb6be78ebd3b75b055e87ffcc868ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b199b980fe93b11510c85ba03397c7",
+    "url": "/supplement_tracker2/index.html"
+  },
+  {
+    "revision": "ffb46a239dfec8d9ae7f",
+    "url": "/supplement_tracker2/static/css/main.e8408901.chunk.css"
+  },
+  {
+    "revision": "db013bfff925a752e4bf",
+    "url": "/supplement_tracker2/static/js/2.d92782c9.chunk.js"
+  },
+  {
+    "revision": "6c46089e713fdf01ec7e89132bc85450",
+    "url": "/supplement_tracker2/static/js/2.d92782c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffb46a239dfec8d9ae7f",
+    "url": "/supplement_tracker2/static/js/main.1236ce84.chunk.js"
+  },
+  {
+    "revision": "8935feead2514f7779d6",
+    "url": "/supplement_tracker2/static/js/runtime-main.cbc54ed5.js"
+  },
+  {
+    "revision": "c53baaf55b85921fda23f89767a09380",
+    "url": "/supplement_tracker2/static/media/2.c53baaf5.jpg"
+  }
+]);
